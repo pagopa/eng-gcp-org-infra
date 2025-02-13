bcd32f6a1628b683f624df2e4cffbaea25a95e41
Pasquale De Vita <pasquale.devita@pagopa.it>
2025-02-13 13:05:47 +0100

    fix

--- src/11_sentinel_auditlogs_setup.tf
+++ src/11_sentinel_auditlogs_setup.tf
@@ -17,25 +17,7 @@ resource "google_pubsub_subscription" "sentinel-subscription" {
   depends_on = [google_pubsub_topic.sentinel-topic]
 }
 
-resource "google_logging_organization_sink" "sentinel-organization-sink" {
-  count            = var.organization_id == "" ? 0 : 1
-  name             = "audit-logs-organization-sentinel-sink"
-  org_id           = var.organization_id
-  destination      = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic_name}"
-  include_children = true
-}
-
-resource "google_project_iam_binding" "log-writer-sentinel-organization-sink" {
-  count   = var.organization_id == "" ? 0 : 1
-  project = data.google_project.project.project_id
-  role    = "roles/pubsub.publisher"
-
-  members = [
-    google_logging_organization_sink.sentinel-organization-sink[0].writer_identity
-  ]
-}
-
-# send pagopa.it organization log to google project log
+# send pagopa.it organization log to google project organization
 resource "google_logging_organization_sink" "sentinel-organization-project-sink" {
   count            = var.organization_id == "" ? 0 : 1
   name             = "audit-logs-organization-project-sink"
@@ -51,5 +33,22 @@ resource "google_project_iam_binding" "log-writer-organization-project-sink" {
 
   members = [
     google_logging_organization_sink.sentinel-organization-project-sink[0].writer_identity
+  ]
+}
+
+# send google project organization log to pub/sub
+resource "google_logging_project_sink" "sentinel-project-pubsub-sink" {
+  count            = var.organization_id == "" ? 0 : 1
+  name             = "audit-logs-organization-sentinel-sink"
+  destination      = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic_name}"
+}
+
+resource "google_project_iam_binding" "log-writer-sentinel-organization-sink" {
+  count   = var.organization_id == "" ? 0 : 1
+  project = data.google_project.project.project_id
+  role    = "roles/pubsub.publisher"
+
+  members = [
+    google_logging_project_sink.sentinel-project-pubsub-sink[0].writer_identity
   ]
 }
